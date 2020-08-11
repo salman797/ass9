cc815302c67c3901ef2c232f7ac758fd614dc4ec
unknown <salman79710@gmail.com>
2020-08-12 01:07:44 +0200

    first commit

---
+++ js/js.js
@@ -0,0 +1,19 @@
+var x=59;
+if(x>100){
+    console.log('invalid number');
+}
+else if(x>=90){
+    console.log('A');
+}
+else if(x>=80){
+    console.log('B');
+}
+else if (x>=70){
+    console.log('C');
+}
+else if(x>=60){
+    console.log('D');
+}
+else{
+    console.log('F')
+}
